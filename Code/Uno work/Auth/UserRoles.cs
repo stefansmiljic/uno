dd61d64d2bf26eadc661e5d759d264e063995b3c
Stefan Smiljić <133028410+stefansmiljic@users.noreply.github.com>
2024-01-19 10:17:46 +0100

    Delete Code/Uno work directory

--- Code/Uno work/Auth/UserRoles.cs
+++
@@ -1,8 +0,0 @@
-namespace JWTAuthentication.NET8._0.Auth
-{
-    public static class UserRoles
-    {
-        public const string Admin = "Admin";
-        public const string User = "User";
-    }
-}
--- Code/Uno work/Models/PersistenceModels/PlayerState.cs
+++
@@ -1,8 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-
-public class PlayerState
-{
-    [Key]
-    public string PlayerId { get; set; }
-    public List<string> Hand { get; set; }
-}
